4bca470c1f47782fec208bdd96dac317e9e6f2e5
Victor Zhang <vwzhang@kc.rr.com>
2026-06-04 11:27:36 -0500

    Fix VS AppHost template generation

--- scripts/Update-Template.ps1
+++ scripts/Update-Template.ps1
@@ -1,6 +1,6 @@
 param(
     [string] $SourceRepository = (Join-Path $PSScriptRoot "..\..\Starter"),
-    [string] $TemplateVersion = "0.1.24",
+    [string] $TemplateVersion = "0.1.25",
     [string] $Configuration = "Release",
     [switch] $SkipSync
 )
